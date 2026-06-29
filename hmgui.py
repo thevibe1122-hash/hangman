8bde0f4cea22bbd5cd7e629a8788aa47d0885b98
salhadin kedir <thevibe1122@gmail.com>
2026-06-28 18:47:07 -0700

    adding word list

--- hmgui.py
+++ hmgui.py
@@ -13,7 +13,36 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Hang man")
         self.setWindowIcon(QIcon("icon.png"))
         self.setGeometry(600, 350, 900, 600)
-        self.word = ["mango", "gorella" , "fucker"]
+        self.word = [
+                "abruptly", "absurd", "abyss", "affix", "askew", "avenue", "awkward", "axiom", 
+                "azure", "bagpipes", "bandwagon", "banjo", "bayou", "beekeeper", "bikini", "blitz", 
+                "blizzard", "boggle", "bookworm", "boxcar", "boxful", "buckaroo", "buffalo", "buffoon", 
+                "buxom", "buzzard", "buzzing", "buzzwords", "caliph", "cobweb", "cockiness", "croquet", 
+                "crypt", "curacao", "cycle", "daiquiri", "dirndl", "disavow", "dizzying", "duplex", 
+                "dwarves", "embezzle", "equip", "espionage", "euouae", "exodus", "faking", "fishhook", 
+                "fixable", "fjord", "flapjack", "flopping", "fluffiness", "flyby", "foxglove", "frazzled", 
+                "frizzled", "fuchsia", "funny", "gabby", "galaxy", "galvanize", "gazebo", "giaour", 
+                "gizmo", "glowworm", "glyph", "gnarly", "gnostic", "gossip", "grogginess", "haiku", 
+                "haphazard", "hyphen", "iatrogenic", "icebox", "injury", "ivory", "ivy", "jackpot", 
+                "jaundice", "jawbreaker", "jaywalk", "jazziest", "jazzy", "jelly", "jigsaw", "jinx", 
+                "jiujitsu", "jockey", "jogging", "joking", "jovial", "joyful", "juicy", "jukebox", 
+                "jumbo", "kayak", "kazoo", "keyhole", "khaki", "kilobyte", "kiosk", "kitsch", 
+                "kiwifruit", "klutz", "knapsack", "larynx", "luxury", "marquis", "matrix", "megahertz", 
+                "microwave", "mnemonic", "mystify", "naphtha", "nightclub", "nowadays", "numbskull", "nymph", 
+                "onyx", "ovary", "oxidize", "oxygen", "pajamas", "peekaboo", "phlegm", "pixel", 
+                "pizazz", "pneumonia", "polka", "pshaw", "psyche", "puppy", "puzzling", "quartz", 
+                "queue", "quips", "quixotic", "quiz", "quizzes", "quorum", "razzmatazz", "rhubarb", 
+                "rhythm", "rickshaw", "schnapps", "scratch", "shiv", "snazzy", "sphinx", "spritz", 
+                "squawk", "staff", "strength", "strengths", "stretch", "stronghold", "stymied", "subway", 
+                "swivel", "syndrome", "thriftless", "thumbscrew", "topaz", "transcript", "transgress", "transplant", 
+                "triphthong", "twelfth", "twelfths", "unknown", "unworthy", "unzip", "uptown", "vaporize", 
+                "vixen", "vodka", "voodoo", "vortex", "voyageur", "walkway", "waltz", "wave", 
+                "wavy", "waxy", "wellspring", "wheezy", "whiskey", "whizzing", "whomever", "wimpy", 
+                "witchcraft", "wizard", "woozy", "wristwatch", "wyvern", "xylophone", "yachtsman", "yippee", 
+                "yoked", "youthful", "yummy", "zephyr", "zigzag", "zigzagging", "zilch", "zipper", 
+                "zodiac", "zombie"
+]
+
         self.wrong_guesses = 0
         self.answer = random.choice(self.word)
         self.hint = ["_"]* len(self.answer)
@@ -134,6 +163,7 @@ class MainWindow(QMainWindow):
             self.l2.setText(f"{display_hints(self.hint)}")
             self.l4.setText("hang man")
             self.b.setText("Submit")
+            self.guessed.clear()
         else:
             self.game_play()
 
